0f426265bffe01746dedbb463d1a75110e4e8cb8
Brun0Nasc <brunonb114@gmail.com>
2025-01-26 19:42:53 -0300

    Componente de atleta

--- src/components/CardAtleta/CardAtleta.tsx
+++ src/components/CardAtleta/CardAtleta.tsx
@@ -1,4 +1,4 @@
-import { Atleta } from "../../types/Atleta";
+import Atleta from "../../types/Atleta";
 
 interface CardAtletaProps {
   atleta: Atleta;
@@ -6,8 +6,20 @@ interface CardAtletaProps {
 
 export function CardAtleta({ atleta }: CardAtletaProps) {
   return (
-    <div className="card-atleta">
-      <h2>{atleta.nome}</h2>
+    <div className="card-atleta" key={atleta.playerId}>
+      <img
+        src={atleta.headshotUrl}
+        alt={`${atleta.firstName} ${atleta.lastName}`}
+        className="img-atleta"
+      />
+      <h3>
+        {atleta.firstName} {atleta.lastName}
+      </h3>
+      <p><strong>Posições:</strong> {atleta.positions}</p>
+      <p><strong>Data de Nascimento:</strong> {atleta.dateBorn}</p>
+      <p><strong>Local de Nascimento:</strong> {atleta.birthPlace}</p>
+      <p><strong>Draft:</strong> {atleta.draftInfo}</p>
+      <button>Favoritar</button>
     </div>
   );
 }
