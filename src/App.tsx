3ebf76191d822b6513b13991f77931387e068fb0
Brun0Nasc <brunonb114@gmail.com>
2025-01-26 15:47:30 -0300

    fixando footer e navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { BrowserRouter } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { AppRoutes } from "./routes";
 import { Footer } from "./components/Footer/Footer";
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <AppRoutes />
-      <Footer />
+      <div className="app">
+        <Navbar />
+        <main className="content">
+          <AppRoutes />
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
